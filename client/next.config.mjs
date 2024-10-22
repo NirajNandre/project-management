e719233f36e99ba63e58e434fe173e9d0277cf4f
Niraj Nandre <nirajnandre11@gmail.com>
2024-10-22 19:24:11 +0530

    redirect images to s3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "workzen-images.s3.amazonaws.com",
+                port: "",
+                pathname: "/**",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
